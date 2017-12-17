2b1aa5f29a8debb448954ada4c7f854df12c9374
Iskorka <camplesjeydi000@gmail.com>
2017-12-17 14:46:59 +0200

    add pop and insert by index; refactor

--- CList.h
+++ CList.h
@@ -8,170 +8,29 @@ template
 class CList
 {
 public:
-    CList() : head_(nullptr), tail_(nullptr), size_(0)
-    {
+    CList();
 
-    }
+    void pushBack(T const &val);
 
-    void pushBack(T val)
-    {
-        Node* newElem = new Node;
+    void pushFront(T const &val);
 
-        newElem->val_ = val;
+    void insert(int index, T const &val);
 
-        if (head_ == nullptr)
-        {
-            newElem->prev_ = nullptr;
+    T popBack();
 
-            head_ = newElem;
-        }
+    T popFront();
 
-        else
-        {
-            newElem->prev_ = tail_;
+    T pop(int index);
 
-            tail_->next_ = newElem;
-        }
+    int inline size() const;
 
-        tail_ = newElem;
+    void show() const;
 
-        ++size_;
-    }
+    void showReversed() const;
 
-    void pushFront(T val)
-    {
-        Node* newElem = new Node;
+    T operator[](int index);
 
-        newElem->val_ = val;
-
-        if (head_ == nullptr)
-        {
-            head_ = newElem;
-        }
-
-        else
-        {
-            newElem->next_ = head_;
-
-            head_->prev_ = newElem;
-
-        }
-
-        head_ = newElem;
-
-        ++size_;
-    }
-
-    T popBack()
-    {
-        Node* temp = tail_;
-
-        T tempVal = temp->val_;
-
-        tail_ = temp->prev_;
-
-        delete temp;
-
-        --size_;
-
-        return tempVal;
-    }
-
-    T popFront()
-    {
-        Node* temp = head_;
-
-        T tempVal = temp->val_;
-
-        head_ = temp->next_;
-
-        delete temp;
-
-        --size_;
-
-        return tempVal;
-    }
-
-    int size()
-    {
-        return size_;
-    }
-
-    void show()
-    {
-        if (head_ == nullptr)
-        {
-            throw std::runtime_error("No elements to show");
-        }
-
-        else
-        {
-            Node* temp = head_;
-
-            while (temp != tail_->next_)
-            {
-                std::cout << temp->val_ << ' ';
-
-                temp = temp->next_;
-            }
-        }
-    }
-
-    void showReversed()
-    {
-        if (head_ == nullptr)
-        {
-            throw std::runtime_error("No elements to show");
-        }
-
-        else
-        {
-            Node* temp = tail_;
-
-            while (temp != head_->prev_)
-            {
-                std::cout << temp->val_ << ' ';
-                temp = temp->prev_;
-            }
-        }
-    }
-
-    T operator[](int index)
-    {
-        if (index >= size_ || index < 0)
-        {
-            throw std::runtime_error("Index out of range");
-        }
-
-        else
-        {
-            Node* temp = head_;
-
-            for (int i=0; i<=index; ++i)
-            {
-                if (i == index)
-                {
-                    return temp->val_;
-                }
-
-                temp = temp->next_;
-            }
-        }
-    }
-
-    ~CList()
-    {
-        Node* temp;
-
-        for (int i=0; i<size_; ++i)
-        {
-            temp = head_->next_;
-
-            delete head_;
-
-            head_ = temp;
-        }
-    }
+    ~CList();
 
 private:
     struct Node
@@ -187,3 +46,269 @@ private:
 };
 
 #endif // CLIST_H
+
+template<typename T>
+CList<T>::CList() : head_(nullptr), tail_(nullptr), size_(0)
+{
+
+}
+
+template<typename T>
+void CList<T>::pushBack(const T &val)
+{
+    Node* newElem = new Node;
+
+    newElem->val_ = val;
+
+    if (head_ == nullptr)
+    {
+        newElem->prev_ = nullptr;
+
+        head_ = newElem;
+    }
+
+    else
+    {
+        newElem->prev_ = tail_;
+
+        tail_->next_ = newElem;
+    }
+
+    tail_ = newElem;
+
+    ++size_;
+}
+
+template<typename T>
+void CList<T>::pushFront(T const &val)
+{
+    Node* newElem = new Node;
+
+    newElem->val_ = val;
+
+    if (head_ == nullptr)
+    {
+        head_ = newElem;
+    }
+
+    else
+    {
+        newElem->next_ = head_;
+
+        head_->prev_ = newElem;
+
+    }
+
+    head_ = newElem;
+
+    ++size_;
+}
+
+template<typename T>
+void CList<T>::insert(int index, const T &val)
+{
+    if (index > size_ || index < 0)
+    {
+        throw std::runtime_error("Index out of range");
+    }
+
+    Node* temp = head_;
+
+    Node* newElem = new Node;
+
+    newElem->val_ = val;
+
+    if (index == 0)
+    {
+        pushFront(val);
+    }
+
+    else if (index == size_)
+    {
+        pushBack(val);
+    }
+
+    else
+    {
+        for (int i=0; i<=index; ++i)
+        {
+            if (i == index)
+            {
+                newElem->next_ = temp;
+
+                newElem->prev_ = temp->prev_;
+
+                temp->prev_->next_ = newElem;
+
+                temp->prev_ = newElem;
+
+            }
+
+            temp = temp->next_;
+        }
+    }
+}
+
+template<typename T>
+T CList<T>::popBack()
+{
+    Node* temp = tail_;
+
+    T tempVal = temp->val_;
+
+    tail_ = temp->prev_;
+
+    delete temp;
+
+    --size_;
+
+    return tempVal;
+}
+
+template<typename T>
+T CList<T>::popFront()
+{
+    Node* temp = head_;
+
+    T tempVal = temp->val_;
+
+    head_ = temp->next_;
+
+    delete temp;
+
+    --size_;
+
+    return tempVal;
+}
+
+template<typename T>
+T CList<T>::pop(int index)
+{
+    if (index > size_ || index < 0)
+    {
+        throw std::runtime_error("Index out of range");
+    }
+
+    else if (index == 0)
+    {
+        popFront();
+    }
+
+    else if (index == size_)
+    {
+        popBack();
+    }
+
+    else
+    {
+        Node* temp = head_;
+
+        for (int i=0; i<=index; ++i)
+        {
+            if (i == index)
+            {
+                T tempVal = temp->val_;
+
+                temp->prev_->next_ = temp->next_;
+
+                temp->next_->prev_ = temp->prev_;
+
+                delete temp;
+
+                return tempVal;
+            }
+
+            temp = temp->next_;
+        }
+    }
+
+    return 0;
+}
+
+template<typename T>
+int CList<T>::size() const
+{
+    return size_;
+}
+
+template<typename T>
+void CList<T>::show() const
+{
+    if (head_ == nullptr)
+    {
+        throw std::runtime_error("No elements to show");
+    }
+
+    else
+    {
+        Node* temp = head_;
+
+        while (temp != tail_->next_)
+        {
+            std::cout << temp->val_ << ' ';
+
+            temp = temp->next_;
+        }
+    }
+}
+
+template<typename T>
+void CList<T>::showReversed() const
+{
+    if (head_ == nullptr)
+    {
+        throw std::runtime_error("No elements to show");
+    }
+
+    else
+    {
+        Node* temp = tail_;
+
+        while (temp != head_->prev_)
+        {
+            std::cout << temp->val_ << ' ';
+            temp = temp->prev_;
+        }
+    }
+}
+
+template<typename T>
+T CList<T>::operator[](int index)
+{
+    if (index >= size_ || index < 0)
+    {
+        throw std::runtime_error("Index out of range");
+    }
+
+    else
+    {
+        Node* temp = head_;
+
+        for (int i=0; i<=index; ++i)
+        {
+            if (i == index)
+            {
+                return temp->val_;
+            }
+
+            temp = temp->next_;
+        }
+    }
+
+    return 0;
+}
+
+template<typename T>
+CList<T>::~CList()
+{
+    Node* temp;
+
+    for (int i=0; i<size_; ++i)
+    {
+        temp = head_->next_;
+
+        delete head_;
+
+        head_ = temp;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -12,7 +12,14 @@ int main()
     l.pushBack(6);
     l.pushBack(142);
 
-    cout << "Elements pushed to back: \n";
+    cout << "Elements ";
+
+    for (int i=0; i<l.size(); ++i)
+    {
+        cout << l[i] << ' ';
+    }
+
+    cout<< "pushed to back: \n\t";
     l.show();
 
     l.pushFront(3);
@@ -20,18 +27,35 @@ int main()
     l.pushFront(1);
     l.pushFront(965);
 
-    cout << endl << "\nElements pushed to front: \n";
+    cout << endl << "\nElements ";
 
+    for (int i=0; i<l.size()/2; ++i)
+    {
+        cout << l[i] << ' ';
+    }
+
+    cout<< "pushed to front: \n\t";
+    l.show();
+
+    l.insert(2, 1024);
+
+    cout << "\n\nElement with value " << l[2] << " inserted into position 2: \n\t";
     l.show();
 
     cout<<endl;
 
     cout << endl << "Popping: \n";
-    cout << "list.popBack(): " << l.popBack() << endl;
+    cout << "\tlist.popBack(): " << l.popBack() << "\n\t\t";
 
     l.show();
     cout << endl;
-    cout << endl << "list.popFront(): " << l.popFront() << endl;
+
+    cout << endl << "\tlist.popFront(): " << l.popFront() << "\n\t\t";
+
+    l.show();
+    cout<< endl;
+
+    cout << endl << "\tlist.pop(1): " << l.pop(1) << "\n\t\t";
 
     l.show();
     cout<< endl;
@@ -41,8 +65,6 @@ int main()
     cout << endl << "Reversed output: \n";
     l.showReversed();
 
-
     cout<<endl<<endl;
-
     return 0;
 }
